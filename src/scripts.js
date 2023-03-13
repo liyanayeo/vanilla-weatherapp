427e621b68373d6b86c1fd905f47784a137a291b
liyanayeo <liyanayeo@gmail.com>
2023-03-13 15:33:19 +0800

    corrected syntax

--- src/scripts.js
+++ src/scripts.js
@@ -1,32 +1,39 @@
-function formatDate (timestamp){
-    let date = new Date(timestamp);
-    let hours = date.getHours();
-    
-    if (hours < 10) {
-    hours `0${minutes}`;
-}
-    
-let minutes = date.getMinutes();
-if (minutes < 10) {
-    minutes `0${minutes}`;
-}
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
 
-    let days = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    let day = date.getDay();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
 
-    return `${day} ${hours}:${minutes}`;
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = date.getDay();
+
+  return `${days[day]} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-  let dateElement = document.querySelector(#date);
+  let dateElement = document.querySelector("#date");
 
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  temperatureElement.innerHTML = Math.round(response.data.main.temperature);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = Math.round(response.data.humidity);
